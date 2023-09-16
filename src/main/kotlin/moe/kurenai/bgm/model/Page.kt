0d016c6d065766ba6840a823132c6fe633593635
Kurenai <kurenai233@yahoo.com>
2023-09-17 02:46:39 +0800

    Fix some issues

--- src/main/kotlin/moe/kurenai/bgm/model/Page.kt
+++ src/main/kotlin/moe/kurenai/bgm/model/Page.kt
@@ -1,5 +1,8 @@
 package moe.kurenai.bgm.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Page<T>(
     val total: Int,
     val limit: Int,
--- src/main/kotlin/moe/kurenai/bgm/request/user/GetCollections.kt
+++ src/main/kotlin/moe/kurenai/bgm/request/user/GetCollections.kt
@@ -2,11 +2,14 @@ package moe.kurenai.bgm.request.user
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import moe.kurenai.bgm.model.Page
 import moe.kurenai.bgm.model.user.UserCollection
 import moe.kurenai.bgm.request.HttpMethod
 import moe.kurenai.bgm.request.Request
 
+@Serializable
 class GetCollections (
     @JsonIgnore
     val username: String,
@@ -25,7 +28,11 @@ class GetCollections (
     override val path: String = "v0/users/$username/collections"
 
     @JsonIgnore
+    @Transient
     override val responseType = jacksonType
+
+    @JsonIgnore
+    override val responseDeserializer = Page.serializer(UserCollection.serializer())
 
     @JsonIgnore
     override val httpMethod = HttpMethod.GET
